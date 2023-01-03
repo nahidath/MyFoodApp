309593eab3963a5fc1c1b3ee60e24c40bd74d43d
nahidath <36660735+nahidath@users.noreply.github.com>
2023-01-03 18:31:53 -0500

    screen more added

--- recipe_app/src/screens/More.tsx
+++ recipe_app/src/screens/More.tsx
@@ -1,11 +1,68 @@
 import React, {FC} from "react";
-import {View, Text} from "react-native";
+import {View, Text, TouchableOpacity, ScrollView} from "react-native";
+import Feather from "react-native-vector-icons/Feather";
+import Separator from "../components/Separator";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import styles from "../stylesheets/More_stylesheet";
 
 
 const More : FC = () => {
     return (
-        <View>
-            <Text>More</Text>
+        <View style={styles.container}>
+            <ScrollView>
+                <View>
+                    <Text style={styles.textTitle}>Paramètres</Text>
+                    <TouchableOpacity style={styles.btnStyle}>
+                        <Feather name={"bell"} size={24} color={"#1C1E21"} />
+                        <Text style={styles.btnStyleText}>Notifications</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnStyle}>
+                        <Feather name={"monitor"} size={24} color={"#1C1E21"} />
+                        <Text style={styles.btnStyleText}>Affichage</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnStyle}>
+                        <Feather name={"globe"} size={24} color={"#1C1E21"} />
+                        <Text style={styles.btnStyleText}>Langue</Text>
+                    </TouchableOpacity>
+                </View>
+                <Separator />
+                <View >
+                    <Text style={styles.textTitle}>Aide</Text>
+                    <TouchableOpacity style={styles.btnStyle}>
+                        <Feather name={"help-circle"} size={24} color={"#1C1E21"} />
+                        <Text style={styles.btnStyleText}>FAQ</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnStyle}>
+                        <Feather name={"mail"} size={24} color={"#1C1E21"} />
+                        <Text style={styles.btnStyleText}>Contact</Text>
+                    </TouchableOpacity>
+                </View>
+                <Separator />
+                <View>
+                    <Text style={styles.textTitle}>Vous aimez MyFoodApp ?</Text>
+                    <TouchableOpacity style={styles.btnStyle}>
+                        <FontAwesome name={"star"} size={24} color={"#1C1E21"} />
+                        <Text style={styles.btnStyleText}>Laissez-nous un avis</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnStyle}>
+                        <FontAwesome name={"share"} size={24} color={"#1C1E21"} />
+                        <Text style={styles.btnStyleText}>Partagez l'application</Text>
+                    </TouchableOpacity>
+                </View>
+                <Separator />
+                <View>
+                    <Text style={styles.textTitle}>A propos</Text>
+                    <TouchableOpacity style={styles.btnStyle}>
+                        <Feather name={"shield"} size={24} color={"#1C1E21"} />
+                        <Text style={styles.btnStyleText}>Politique de confidentialité</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnStyle}>
+                        <FontAwesome name={"file"} size={24} color={"#1C1E21"} />
+                        <Text style={styles.btnStyleText}>Conditions d'utilisation</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+
         </View>
     );
 }
---
+++ recipe_app/src/stylesheets/More_stylesheet.tsx
@@ -0,0 +1,33 @@
+import {StyleSheet} from "react-native";
+
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#c7b922',
+        flexDirection: 'column',
+    },
+    textTitle: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#1C1E21',
+        margin: 10,
+    },
+    btnStyle: {
+        padding: 10,
+        backgroundColor: '#fff',
+        borderRadius: 15,
+        height: 60,
+        margin: 10,
+        flexDirection: 'row',
+    },
+    btnStyleText: {
+        fontSize: 20,
+        // fontWeight: 'bold',
+        color: '#1C1E21',
+        paddingLeft: 10,
+        alignContent: 'center',
+        justifyContent: 'center',
+    },
+
+});
